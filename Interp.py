c0e7cc407a14337309e62bc2555646a35fe09c8c
Goobley <2087801o@student.gla.ac.uk>
2020-04-09 20:02:54 +0100

    Implemented a first pass at the "NASA" radyn atoms. See note in MsLightweaver.py regarding maybe needing LTE hPops for initial CH rates for Ca. May just be bug in my code

--- Interp.py
+++ Interp.py
@@ -12,13 +12,13 @@ def compute_cmass(atmost: Atmost) -> np.ndarray:
 def interp_to_const_cmass_grid(atmost: Atmost, cmass: np.ndarray, newGrid: np.ndarray, interp_fn=np.interp) -> Atmost:
     computeBheat = (atmost.bheat1.shape[0] != 0)
 
-    z1 = np.zeros_like(atmost.z1)
-    d1 = np.zeros_like(atmost.d1)
-    ne1 = np.zeros_like(atmost.ne1)
-    tg1 = np.zeros_like(atmost.tg1)
-    vz1 = np.zeros_like(atmost.vz1)
-    bheat1 = np.zeros_like(atmost.bheat1)
-    nh1 = np.zeros_like(atmost.nh1)
+    z1 = np.zeros((atmost.time.shape[0], newGrid.shape[0]))
+    d1 = np.zeros((atmost.time.shape[0], newGrid.shape[0]))
+    ne1 = np.zeros((atmost.time.shape[0], newGrid.shape[0]))
+    tg1 = np.zeros((atmost.time.shape[0], newGrid.shape[0]))
+    vz1 = np.zeros((atmost.time.shape[0], newGrid.shape[0]))
+    bheat1 = np.zeros((atmost.time.shape[0], newGrid.shape[0]))
+    nh1 = np.zeros((atmost.time.shape[0], 6, newGrid.shape[0]))
 
     for i in range(z1.shape[0]):
         z1[i] = np.interp(newGrid, cmass[i], atmost.z1[i])
@@ -31,4 +31,6 @@ def interp_to_const_cmass_grid(atmost: Atmost, cmass: np.ndarray, newGrid: np.nd
         for j in range(nh1.shape[1]):
             nh1[i,j] = np.interp(newGrid, cmass[i], atmost.nh1[i,j])
 
-    return Atmost(atmost.grav, atmost.tau2, atmost.vturb, atmost.time, atmost.dt, z1, d1, ne1, tg1, vz1, nh1, bheat1, cgs=atmost.cgs)
+    vturb = np.interp(newGrid, cmass[0], atmost.vturb)
+
+    return Atmost(atmost.grav, atmost.tau2, vturb, atmost.time, atmost.dt, z1, d1, ne1, tg1, vz1, nh1, bheat1, cgs=atmost.cgs)
--- MsLightweaver.py
+++ MsLightweaver.py
@@ -1,17 +1,17 @@
 import pickle
 import numpy as np
 import matplotlib.pyplot as plt
-from lightweaver.rh_atoms import C_atom, O_atom, OI_ord_atom, Si_atom, Al_atom, Fe_atom, FeI_atom, MgII_atom, N_atom, Na_atom, S_atom, CaII_atom
+from lightweaver.rh_atoms import H_6_atom, C_atom, O_atom, OI_ord_atom, Si_atom, Al_atom, Fe_atom, FeI_atom, MgII_atom, N_atom, Na_atom, S_atom, CaII_atom
 from lightweaver.atmosphere import Atmosphere, ScaleType
 from lightweaver.atomic_set import RadiativeSet, SpeciesStateTable
 from lightweaver.atomic_table import get_global_atomic_table
 from lightweaver.molecule import MolecularTable
 from lightweaver.LwCompiled import LwContext
-from lightweaver.utils import InitialSolution, planck
+from lightweaver.utils import InitialSolution, planck, NgOptions, ConvergenceError
 import lightweaver.constants as Const
 from typing import List
 from copy import deepcopy
-from MsLightweaverAtoms import H_6, CaII, He_9
+from MsLightweaverAtoms import H_6, CaII, He_9, H_6_nasa, CaII_nasa
 import os
 import os.path as path
 import time
@@ -19,8 +19,12 @@ from notify_run import Notify
 from radynpy.matsplotlib import OpcFile
 from radynpy.utils import hydrogen_absorption
 from numba import njit
+from pathlib import Path
 
-OutputDir = 'TimestepsRadynCeaFinal/'
+OutputDir = 'TimestepsNasa/'
+Path(OutputDir).mkdir(parents=True, exist_ok=True)
+Path(OutputDir + '/Rfs').mkdir(parents=True, exist_ok=True)
+Path(OutputDir + '/ContFn').mkdir(parents=True, exist_ok=True)
 
 # def planck_nu_freq(nu, t):
 #     x = Const.HPlanck * nu / Const.KBoltzmann / t
@@ -67,8 +71,12 @@ class MsLightweaverManager:
             self.atmos.convert_scales()
             self.atmos.quadrature(5)
 
-            self.aSet = RadiativeSet([H_6(), CaII(), He_9(), C_atom(), O_atom(), Si_atom(), Fe_atom(), MgII_atom(), N_atom(), Na_atom(), S_atom()])
+            self.aSet = RadiativeSet([H_6_nasa(), CaII_nasa(), He_9(), C_atom(), O_atom(), Si_atom(), Fe_atom(), MgII_atom(), N_atom(), Na_atom(), S_atom()])
             self.aSet.set_active('H', 'Ca')
+            # NOTE(cmo): Radyn seems to compute the collisional rates once per
+            # timestep(?) and we seem to get a much better agreement for Ca
+            # with the CH rates when H is set to LTE for the initial timestep.
+            # Might be a bug in my implementation though.
 
             self.spect = self.aSet.compute_wavelength_grid()
 
@@ -76,46 +84,47 @@ class MsLightweaverManager:
             self.eqPops = self.aSet.compute_eq_pops(self.atmos, self.mols)
             # self.eqPops = self.aSet.iterate_lte_ne_eq_pops(self.mols, self.atmos)
 
-            self.ctx = LwContext(self.atmos, self.spect, self.eqPops, initSol=InitialSolution.Lte, conserveCharge=False, Nthreads=8)
+            # self.ctx = LwContext(self.atmos, self.spect, self.eqPops, initSol=InitialSolution.Lte, conserveCharge=False, Nthreads=12, ngOptions=NgOptions(3, 7, 50))
+            self.ctx = LwContext(self.atmos, self.spect, self.eqPops, initSol=InitialSolution.Lte, conserveCharge=False, Nthreads=12)
 
-        self.atmos.bHeat = self.atmost['bheat1'][0]
+        self.atmos.bHeat = np.ones_like(self.atmost['bheat1'][0]) * 1e-20
         self.atmos.hPops = self.eqPops['H']
 
         # NOTE(cmo): Set up background
-        self.opc = OpcFile('opctab_cmo_mslw.dat')
-        # self.opc = OpcFile()
-        opcWvl = self.opc.wavel
-        self.opcWvl = opcWvl
-        # NOTE(cmo): Find mapping from wavelength array to opctab array, with
-        # constant background over the region of each line. Are overlaps a
-        # problem here? Probably -- but let's see the spectrum in practice
-        # The record to be used is the one in self.wvlIdxs + 4 due to the data
-        # layout in the opctab
-        self.wvlIdxs = np.ones_like(self.spect.wavelength, dtype=np.int64) * -1
-        lineCores = []
-        for a in self.aSet.activeSet:
-            for l in a.lines:
-                lineCores.append(l.lambda0 * 10)
-        lineCores = np.array(lineCores)
-        lineCoreIdxs = np.zeros_like(lineCores)
-        for i, l in enumerate(lineCores):
-            closestIdx = np.argmin(np.abs(opcWvl - l))
-            lineCoreIdxs[i] = closestIdx
+        # self.opc = OpcFile('opctab_cmo_mslw.dat')
+        # # self.opc = OpcFile()
+        # opcWvl = self.opc.wavel
+        # self.opcWvl = opcWvl
+        # # NOTE(cmo): Find mapping from wavelength array to opctab array, with
+        # # constant background over the region of each line. Are overlaps a
+        # # problem here? Probably -- but let's see the spectrum in practice
+        # # The record to be used is the one in self.wvlIdxs + 4 due to the data
+        # # layout in the opctab
+        # self.wvlIdxs = np.ones_like(self.spect.wavelength, dtype=np.int64) * -1
+        # lineCores = []
+        # for a in self.aSet.activeSet:
+        #     for l in a.lines:
+        #         lineCores.append(l.lambda0 * 10)
+        # lineCores = np.array(lineCores)
+        # lineCoreIdxs = np.zeros_like(lineCores)
+        # for i, l in enumerate(lineCores):
+        #     closestIdx = np.argmin(np.abs(opcWvl - l))
+        #     lineCoreIdxs[i] = closestIdx
 
-        for a in self.aSet.activeSet:
-            for l in a.lines:
-                # closestIdx = np.argmin((opcWvl - l.lambda0*10)**2)
-                closestCore = np.argmin(np.abs((l.wavelength * 10)[:, None] - lineCores), axis=1)
-                closestIdx = lineCoreIdxs[closestCore]
-                sub = find_subarray(self.spect.wavelength, l.wavelength)
-                self.wvlIdxs[sub:sub + l.wavelength.shape[0]] = closestIdx
-        for i, v in enumerate(self.wvlIdxs):
-            if v >= 0:
-                continue
+        # for a in self.aSet.activeSet:
+        #     for l in a.lines:
+        #         # closestIdx = np.argmin((opcWvl - l.lambda0*10)**2)
+        #         closestCore = np.argmin(np.abs((l.wavelength * 10)[:, None] - lineCores), axis=1)
+        #         closestIdx = lineCoreIdxs[closestCore]
+        #         sub = find_subarray(self.spect.wavelength, l.wavelength)
+        #         self.wvlIdxs[sub:sub + l.wavelength.shape[0]] = closestIdx
+        # for i, v in enumerate(self.wvlIdxs):
+        #     if v >= 0:
+        #         continue
 
-            closestIdx = np.argmin(np.abs(opcWvl - self.spect.wavelength[i]*10))
-            self.wvlIdxs[i] = closestIdx
-        self.opctabIdxs = self.wvlIdxs + 4
+        #     closestIdx = np.argmin(np.abs(opcWvl - self.spect.wavelength[i]*10))
+        #     self.wvlIdxs[i] = closestIdx
+        # self.opctabIdxs = self.wvlIdxs + 4
 
         # NOTE(cmo): Compute initial background opacity
         # np.save('chi.npy', self.ctx.background.chi)
@@ -185,6 +194,8 @@ class MsLightweaverManager:
             if self.ctx.crswDone and dJ < 3e-3 and delta < 1e-3:
                 print('Stat eq converged in %d iterations' % (i+1))
                 break
+        else:
+            raise ConvergenceError('Stat Eq did not converge.')
 
     def update_height(self):
         height = self.atmos.height
@@ -214,16 +225,35 @@ class MsLightweaverManager:
         self.ctx.update_deps()
         # self.opac_background()
 
-    def time_dep_step(self, nSubSteps=100, popsTol=1e-3, JTol=3e-3):
+    def time_dep_step(self, nSubSteps=200, popsTol=1e-3, JTol=3e-3):
         dt = self.atmost['dt'][self.idx+1]
+        # self.ctx.spect.J[:] = 0.0
 
         prevState = None
         for sub in range(nSubSteps):
             dJ = self.ctx.formal_sol_gamma_matrices()
-            delta, prevState = self.ctx.time_dep_update(dt, prevState)
+            if sub > 2:
+                delta, prevState = self.ctx.time_dep_update(dt, prevState)
 
-            if delta < popsTol and dJ < JTol:
-                break
+                if delta < popsTol and dJ < JTol:
+                    break
+        else:
+            self.ctx.depthData.fill = True
+            self.ctx.formal_sol_gamma_matrices()
+            self.ctx.depthData.fill = False
+            
+            sourceData = {'chi': np.copy(self.ctx.depthData.chi),
+                        'eta': np.copy(self.ctx.depthData.eta),
+                        'chiBg': np.copy(self.ctx.background.chi),
+                        'etaBg': np.copy(self.ctx.background.eta),
+                        'scaBg': np.copy(self.ctx.background.sca),
+                        'J': np.copy(self.ctx.spect.J)
+                        }
+            with open(OutputDir + 'Fails.txt', 'a') as f:
+                f.write('%d, %.4e %.4e\n' % (self.idx, delta, dJ))
+
+            with open(OutputDir + 'NonConvergenceData_%.6d.pickle' % (self.idx), 'wb') as pkl:
+                pickle.dump(sourceData, pkl)
         # self.ctx.time_dep_conserve_charge(prevState)
 
 filesInOutDir = [f for f in os.listdir(OutputDir) if f.startswith('Step_')]
@@ -282,11 +312,15 @@ if startingCtx is None:
     with open(OutputDir + 'StartingContext.pickle', 'wb') as pkl:
         pickle.dump(ms.ctx, pkl)
 
-for i in range(ms.atmost['time'].shape[0] - 1):
+maxSteps = ms.atmost['time'].shape[0] - 1
+maxSteps = 5
+ms.atmos.bHeat[:] = ms.atmost['bheat1'][0]
+for i in range(maxSteps):
     stepStart = time.time()
     if i != 0:
         ms.increment_step()
     ms.time_dep_step(popsTol=1e-3, JTol=5e-3)
+    ms.ctx.clear_ng()
     save_timestep(i+1)
     stepEnd = time.time()
     print('-------')
